aa79289b94c6ec13770f1e3a6d19faca4d4a925c
Mateus <mateus0699@hotmail.com>
2026-03-08 03:06:18 -0300

    feat: aplica migrations automaticamente no startup

--- Program.cs
+++ Program.cs
@@ -37,5 +37,11 @@ app.UseHttpsRedirection();
 app.UseCors("AllowAngular");
 app.UseAuthorization();
 app.MapControllers();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
 
